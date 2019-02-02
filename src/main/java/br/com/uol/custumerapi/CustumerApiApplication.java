9489a60a0d144abe705755cad0aa8cf37042aaaf
Gabriel Kirsten Menezes <gabriel.kirsten@hotmail.com>
2019-02-02 18:23:25 -0300

    Init project Spring Initializr

---
+++ src/main/java/br/com/uol/custumerapi/CustumerApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.uol.custumerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustumerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustumerApiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/uol/custumerapi/CustumerApiApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.uol.custumerapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustumerApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
